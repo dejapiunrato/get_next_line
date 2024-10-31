34b33b7f43da740262871d4e637cd69f5b86c951
Piesito <83318869+dejapiunrato@users.noreply.github.com>
2024-10-31 01:47:10 +0100

    Update get_next_line_utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -11,3 +11,69 @@
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+size_t	ft_strlen(char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(char *s)
+{
+	char	*str;
+	int		i;
+
+	str = malloc(ft_strlen(s) + 1);
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (*s)
+		str[i++] = *s++;
+	str[i] = '\0';
+	return (str);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*s;
+	int		i;
+
+	s = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!s)
+		return (NULL);
+	i = 0;
+	while (*s1)
+		s[i++] = *s1++;
+	while (*s2)
+		s[i++] = *s2++;
+	s[i] = '\0';
+	return (s);
+}
+
+char	*ft_substr(char *s, unsigned int start, int len)
+{
+	char	*sub;
+	int		i;
+
+	if (!s)
+		return (NULL);
+	if (start > ft_strlen(s))
+		return (NULL);
+	if (ft_strlen(s) - start < len)
+		len = ft_strlen(s) - start;
+	sub = (char *)malloc(sizeof(char) * len + 1);
+	if (!s)
+		return (NULL);
+	i = 0;
+	while (i < len)
+		sub[i++] = s[start + i];
+	return (sub);
+
+}
